adobe/aemanalyser-maven-plugin

commit 84b60a7a54431096aa391143c6b0aa9ce466ab59

Allow to specify additional content packages to consider (#297)
* Allow to specify additional content packages to consider

This closes #219

* Take type as is from coordinates

Map it prior to resolving with Maven resolver

aemanalyser-maven-plugin/src/main/java/com/adobe/aem/analyser/mojos/AemAnalyseMojo.java
@@ -93,11 +93,21 @@ public class AemAnalyseMojo extends AbstractAnalyseMojo {
 
     /**
      * Analyzes the given list of content package files.
-     * If this is configured, only these files are validated, and not the main project artifact or dependencies.
+     * If this is configured, only these files are validated (and potentially {@link #additionalContentPackageArtifacts}), 
+     * but not the main project artifact or dependencies.
      * The files must be located inside the Maven project directory (e.g. src or target folder). 
      */
     @Parameter
     private List<File> contentPackageFiles;
+
+    /**
+     * Additional content package artifacts to be considered in the analysis given as list of Maven coordinates/ids in format
+     * {@code groupId:artifactId[:packaging[:classifier]]:version}).
+     * This is useful for container packages which depend on other container packages deployed together in Cloud Manager
+     * (e.g. composed via git submodules) as otherwise analysis may fail.
+     */
+    @Parameter
+    private List<String> additionalContentPackageArtifacts;
 
     /**
      * Get the output directory for the content package converter
@@ -155,6 +165,9 @@ public class AemAnalyseMojo extends AbstractAnalyseMojo {
         final Map<String, File> packages = new LinkedHashMap<>();
         for(final Artifact contentPackage: getContentPackages()) {
             final File source = contentPackage.getFile();
+            if (source == null) {
+                throw new MojoExecutionException("Content package " + contentPackage + " has no file attached");
+            }
             packages.put(contentPackage.getId().toString(), source);
         }
         try {
@@ -173,19 +186,20 @@ public class AemAnalyseMojo extends AbstractAnalyseMojo {
      * @throws MojoExecutionException If anything goes wrong, for example no content packages are found
      */
     private List<Artifact> getContentPackages() throws MojoExecutionException {
+        final List<Artifact> result = new ArrayList<>();
         if (!Constants.PACKAGING_AEM_ANALYSE.equals(project.getPackaging())) {
             if (contentPackageFiles != null && !contentPackageFiles.isEmpty()) {
+                getLog().info("Using content packages from contentPackageFiles");
                 validateContentPackageFiles(contentPackageFiles);
-                return contentPackageFiles.stream()
+                contentPackageFiles.stream()
                         .map(this::contentPackageFileToArtifact)
-                        .collect(Collectors.toList());
-                        
+                        .forEach(result::add);
             } else if (classifier != null) {
                 // look for attached artifact with given classifier
                 for (Artifact artifact : project.getAttachedArtifacts()) {
                     if (classifier.equals(artifact.getClassifier())) {
                         getLog().info("Using attached artifact with classifier '" + classifier + "' as content package: " + project.getArtifact());
-                        return Collections.singletonList(artifact);
+                        result.add(artifact);
                     }
                 }
                 throw new MojoExecutionException("No attached artifact with classifier " + classifier + " found for project.");
@@ -208,11 +222,10 @@ public class AemAnalyseMojo extends AbstractAnalyseMojo {
                     targetArtifact.setFile(target);
                     return Collections.singletonList(targetArtifact);
                 }
-                return Collections.singletonList(project.getArtifact());
+                result.add(project.getArtifact());
             }
         } else {
-            final List<Artifact> result = new ArrayList<>();
-            for( final Dependency d : project.getDependencies()) {
+            for (final Dependency d : project.getDependencies()) {
                 if (Constants.PACKAGING_ZIP.equals(d.getType()) || Constants.PACKAGING_CONTENT_PACKAGE.equals(d.getType())) {
                     // If a dependency is of type 'zip' it is assumed to be a content package
                     final Artifact artifact = getOrResolveArtifact(new ArtifactId(d.getGroupId(), d.getArtifactId(), d.getVersion(), d.getClassifier(), d.getType()));
@@ -222,9 +235,19 @@ public class AemAnalyseMojo extends AbstractAnalyseMojo {
             if (result.isEmpty()) {
                 throw new MojoExecutionException("No content packages found for project.");
             }
-            getLog().info("Found content packages from dependencies: " + result);
-            return result;
+            getLog().info("Using content packages from dependencies: " + result);
         }
+        if (additionalContentPackageArtifacts != null) {
+            additionalContentPackageArtifacts.stream()
+                .map(ArtifactId::fromMvnId)
+                .map(a -> new ArtifactId(a.getGroupId(), a.getArtifactId(), a.getVersion(), a.getClassifier(), a.getType()))
+                .map(this::getOrResolveArtifact)
+                .forEach(a -> {
+                        getLog().info("Considering additional content package: " + a);
+                        result.add(a); 
+                    });
+        }
+        return result;
     }
 
     /**

aemanalyser-maven-plugin/src/main/java/com/adobe/aem/analyser/mojos/AbstractAemMojo.java
@@ -21,6 +21,8 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.maven.RepositoryUtils;
 import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.handler.ArtifactHandler;
+import org.apache.maven.artifact.handler.manager.ArtifactHandlerManager;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Parameter;
@@ -53,6 +55,12 @@ public abstract class AbstractAemMojo extends AbstractMojo {
 
     @Component
     protected RepositorySystem repoSystem;
+
+    /**
+     * The artifact manager to resolve artifacts
+     */
+    @Component
+    protected ArtifactHandlerManager artifactHandlerManager;
 
     @Parameter(defaultValue = "${repositorySystemSession}", readonly = true, required = true)
     protected RepositorySystemSession repoSession;
@@ -107,7 +115,8 @@ public abstract class AbstractAemMojo extends AbstractMojo {
             if ( result == null ) {
                 result = findArtifact(id, project.getArtifacts());
                 if ( result == null ) {
-                    ArtifactRequest req = new ArtifactRequest(new org.eclipse.aether.artifact.DefaultArtifact(id.toMvnId()), project.getRemoteProjectRepositories(), null);
+                    // map Maven type to simple extension
+                    ArtifactRequest req = new ArtifactRequest(new org.eclipse.aether.artifact.DefaultArtifact(resolveTypeInArtifactId(id).toMvnId()), project.getRemoteProjectRepositories(), null);
                     try {
                         ArtifactResult resolutionResult = repoSystem.resolveArtifact(repoSession, req);
                         result = RepositoryUtils.toArtifact(resolutionResult.getArtifact());
@@ -121,7 +130,24 @@ public abstract class AbstractAemMojo extends AbstractMojo {
 
         return result;
     }
-    
+
+    /**
+     * Resolves the type of the given artifact to its extension. Noop in case there is no artifact handler for the given type.
+     * @param id
+     * @return the id with the resolved type
+     */
+    private ArtifactId resolveTypeInArtifactId(ArtifactId id) {
+        if (id.getType() == null) {
+            return id;
+        }
+        ArtifactHandler artifactHandler = artifactHandlerManager.getArtifactHandler(id.getType());
+        if (artifactHandler != null) {
+            return id.changeType(artifactHandler.getExtension());
+        } else {
+            return id;
+        }
+    }
+
     /**
      * Get a resolved feature
      *

aemanalyser-maven-plugin/src/main/java/com/adobe/aem/analyser/mojos/AbstractAnalyseMojo.java
@@ -78,12 +78,6 @@ public abstract class AbstractAnalyseMojo extends AbstractAemMojo {
     protected boolean strictVersionValidation;
 
     /**
-     * The artifact manager to resolve artifacts
-     */
-    @Component
-    protected ArtifactHandlerManager artifactHandlerManager;
-
-    /**
      * The maven session
      */
     @Parameter(property = "session", readonly = true, required = true)

commit ca04d67f93f86172f8048c07b0aa17dfd5dab4eb

Only throw MojoExecutionException when classifier is not found
This fixes a regression introduced with
https://github.com/adobe/aemanalyser-maven-plugin/commit/84b60a7a54431096aa391143c6b0aa9ce466ab59
as that lead to the fact that
"org.apache.maven.plugin.MojoExecutionException: No attached artifact
with classifier cloud found for project." was always emitted, even if
the attached classifier was in fact found.

This closes #340

aemanalyser-maven-plugin/src/main/java/com/adobe/aem/analyser/mojos/AemAnalyseMojo.java
@@ -202,7 +202,9 @@ public class AemAnalyseMojo extends AbstractAnalyseMojo {
                         result.add(artifact);
                     }
                 }
-                throw new MojoExecutionException("No attached artifact with classifier " + classifier + " found for project.");
+                if (result.isEmpty()) {
+                    throw new MojoExecutionException("No attached artifact with classifier " + classifier + " found for project.");
+                }
             } else {
                 // Use the current project artifact as the content package
                 getLog().info("Using current project as content package: " + project.getArtifact());