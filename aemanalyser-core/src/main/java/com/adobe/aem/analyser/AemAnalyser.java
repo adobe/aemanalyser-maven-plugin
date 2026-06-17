864941793c09a519a770ca17074b8ffada275a23
Niek Raaijmakers <raaijmak@adobe.com>
2026-06-17 12:49:44 +0200

    Revert breaking build

--- aemanalyser-core/src/main/java/com/adobe/aem/analyser/AemAnalyser.java
+++ aemanalyser-core/src/main/java/com/adobe/aem/analyser/AemAnalyser.java
@@ -71,8 +71,7 @@ public class AemAnalyser {
         + ",repoinit"
         + ",content-packages-validation"
         + ",configurations-basic"
-        + ",aem-provider-type"
-        + ",repoinit-conflicts";
+        + ",aem-provider-type";
 
     private static final String CONTENT_PACKAGE_ORIGINS = "content-package-origins";
     private static final String CONFIGURATION_ORIGINS = Configuration.CONFIGURATOR_PREFIX.concat(CONTENT_PACKAGE_ORIGINS);
